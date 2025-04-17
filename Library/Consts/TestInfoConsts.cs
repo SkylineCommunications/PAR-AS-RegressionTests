f18671682530b603ec826291a690e412f95ad1c4
Alex Johnson <llalexjohnsonll@gmail.com>
2025-04-17 12:15:57 -0500

    changed test info consts/first test xml name

--- Library/Consts/TestInfoConsts.cs
+++ Library/Consts/TestInfoConsts.cs
@@ -7,8 +7,8 @@
 
 	internal static class TestInfoConsts
 	{
-		internal static string Contact => "@skyline.be"; // TODO: The 'Contact' field must be your squad or domain e-mail. No Individual e-mails are allowed.
+		internal static string Contact => "squad.deploy-genesis@skyline.be"; // TODO: The 'Contact' field must be your squad or domain e-mail. No Individual e-mails are allowed.
 
-		internal static List<int> ProjectIds => new List<int> { -1 }; // TODO: Change project ID
+		internal static List<int> ProjectIds => new List<int> { 18432 }; // TODO: Change project ID
 	}
 }
